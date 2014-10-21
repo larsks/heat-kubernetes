b2ad1fbd1f733dc4e702912175dad22b66a95469
Lars Kellogg-Stedman <lars@redhat.com>
2014-10-20 23:54:02 -0400

    corrections to get-image.sh script
    - fixed an order-of-operations issue that could have resulted in
      conflicts between docker-io and legacy "docker" package
    - customize a copy of downloaded image rather than the original (to make
      it easier to start from scratch)
    - make sure to clean up yum cache

--- get-image.sh
+++ get-image.sh
@@ -4,21 +4,28 @@
 # curl, libguestfs-tools-c
 
 IMAGE=Fedora-x86_64-20-20140618-sda.qcow2
+TARGET=fedora-20-k8s.qcow2
 
 if ! [ -f "$IMAGE" ]; then
+    echo "Downloading $IMAGE"
     curl -O http://archive.fedoraproject.org/pub/alt/openstack/20/x86_64/$IMAGE
 fi
 
-PACKAGES="jq,dnf,dnf-plugins-core,openvswitch,bridge-utils,docker-io,git\
+echo "Copying $IMAGE to $TARGET"
+cp "$IMAGE" $TARGET
+
+PACKAGES="jq,dnf,bridge-utils,docker-io,git\
 ,python-netifaces,python-requests,tcpdump,python-setuptools"
 
 virt-customize \
-    --add $IMAGE \
-    --update \
-    --install $PACKAGES \
+    --add $TARGET \
+    --install dnf,dnf-plugins-core \
     --run-command "dnf -y copr enable walters/atomic-next" \
     --run-command "dnf -y copr enable larsks/fakedocker" \
+    --update \
+    --install $PACKAGES \
     --install kubernetes \
+    --run-command "yum clean all" \
     --root-password password:password
 
 # SELinux relabeling requires virt-customize to have networking disabled
