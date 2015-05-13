7f92dd01893ae9b47d90dfa6ad1881e1884854e2
Lars Kellogg-Stedman <lars@redhat.com>
2015-05-13 11:10:21 -0400

    configure docker storage correctly
    The changes made by the configure-docker-storage.sh fragment were being
    overwritten by the docker-storage-setup script that atomic runs at boot.
    This patch updates configure-docker-storage.sh such that it will
    generate configuration for docker-storage-setup, which will in turn
    generate the correct /etc/sysconfig/docker-storage.
    
    Closes #24
    
    Change-Id: Ib86b0238b7b82bd28343ddfd385c31a73dc5af19

--- fragments/configure-docker-storage.sh
+++ fragments/configure-docker-storage.sh
@@ -1,8 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
 . /etc/sysconfig/heat-params
 
 DOCKER_DEV=/dev/disk/by-id/virtio-${DOCKER_VOLUME:0:20}
+
+attempts=60
+while [[ ! -b $DOCKER_DEV && $attempts != 0 ]]; do
+	echo "waiting for disk $DOCKER_DEV"
+	sleep 0.5
+	udevadm trigger
+	let attempts--
+done
 
 if ! [ -b $DOCKER_DEV ]; then
 	echo "ERROR: device $DOCKER_DEV does not exist" >&2
@@ -11,10 +19,8 @@ fi
 
 pvcreate $DOCKER_DEV
 vgcreate docker $DOCKER_DEV
-lvcreate --wipesignatures y -n data docker -l 95%VG
-lvcreate --wipesignatures y -n metadata docker -l 5%VG
 
-cat > /etc/sysconfig/docker-storage <<EOF
-DOCKER_STORAGE_OPTIONS="--storage-opt dm.blkdiscard=false --storage-opt dm.metadatadev=/dev/docker/metadata --storage-opt dm.datadev=/dev/docker/data --storage-opt dm.fs=xfs"
+cat > /etc/sysconfig/docker-storage-setup <<EOF
+VG=docker
 EOF
 
