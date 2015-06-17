62f95d849255b46f4fff1b8751607607a34b9e7e
Lars Kellogg-Stedman <lars@redhat.com>
2015-06-17 15:20:28 -0400

    KUBE_MASTER moved from apiserver to config
    Change-Id: I34703d82277df0cb72a713cb3c1145a5f2eaa2c8

--- fragments/configure-kubernetes-node.sh
+++ fragments/configure-kubernetes-node.sh
@@ -11,6 +11,7 @@ myip_last_octet=${myip##*.}
 sed -i '
 /^KUBE_ALLOW_PRIV=/ s/=.*/="--allow_privileged='"$KUBE_ALLOW_PRIV"'"/
 /^KUBE_ETCD_SERVERS=/ s|=.*|="--etcd_servers=http://'"$KUBE_MASTER_IP"':4001"|
+/^KUBE_MASTER=/ s/=.*/="--master='"$KUBE_MASTER_IP"':8080"/
 ' /etc/kubernetes/config
 
 sed -i '
@@ -18,10 +19,6 @@ sed -i '
 /^KUBELET_HOSTNAME=/ s/=.*/="--hostname_override='"$myip"'"/
 /^KUBELET_API_SERVER=/ s/=.*/="--api_servers='"$KUBE_MASTER_IP"':8080"/
 ' /etc/kubernetes/kubelet
-
-sed -i '
-/^KUBE_MASTER=/ s/=.*/="--master='"$KUBE_MASTER_IP"':8080"/
-' /etc/kubernetes/apiserver
 
 sed -i '
 /^FLANNEL_ETCD=/ s|=.*|="http://'"$KUBE_MASTER_IP"':4001"|
